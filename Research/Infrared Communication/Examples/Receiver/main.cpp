de0443aad25e6c70ee7ac45a13d1650d86cf98f0
Jochem <jochem.vankranenburg@student.hu.nl>
2019-09-22 14:43:36 +0200

    Added Examples

---
+++ Research/Infrared Communication/Examples/Receiver/main.cpp
@@ -0,0 +1,15 @@
+#include "hwlib.hpp"
+#include "transceiver.hpp"
+
+int main( void ){	
+   hwlib::wait_ms( 500 );
+
+   auto irReceiverPin = hwlib::target::pin_in(hwlib::target::pins::d5);
+   auto irReceiver = receiver(irReceiverPin);
+
+   for(;;){
+      if(irReceiver.dataAvailable()){
+         hwlib::cout << irReceiver.readData() << hwlib::endl;
+      }
+   }
+}
---
+++ Research/Infrared Communication/Examples/Transmitter/main.cpp
@@ -0,0 +1,15 @@
+#include "hwlib.hpp"
+#include "transceiver.hpp"
+
+int main( void ){	
+	hwlib::wait_ms( 500 );
+
+	auto irTransmitter = transmitter();
+
+	const uint16_t data = 65333;
+
+   	for(;;){
+    	irTransmitter.sendData(data);
+		hwlib::wait_ms(2000);
+   	}
+}
