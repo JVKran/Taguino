48ffa9910ae9a95e90242b484b7c74111e7c93ac
Jochem <jochem.vankranenburg@student.hu.nl>
2019-10-10 11:15:32 +0200

    Shifting Distance and Health

--- Research/DistanceIr/Test/main.cpp
+++ Research/DistanceIr/Test/main.cpp
@@ -4,22 +4,32 @@
 
 int main( void ){	
 	hwlib::wait_ms( 500 );
+
 	auto triggerPin = hwlib::target::pin_out(hwlib::target::pins::d49);
    	auto echoPin = hwlib::target::pin_in(hwlib::target::pins::d47);
 	auto distanceSensor = HCSR04(triggerPin, echoPin);
 
 	auto irTransmitter = transmitter();
 
-	uint16_t data = 0000;
-	const uint8_t lives = 9;
-	auto dist = 0;
-	//distanceSensor.getDistance();
-	data |= dist;
-	data |= lives >>9;
+	uint8_t dist = 0;
+	int8_t health = 80;
+	uint16_t data = 0;
 
    	for(;;){
-    	irTransmitter.sendData(data);
-		hwlib::cout <<data;
-		hwlib::wait_ms(2000);
+   		data = 0000;
+		dist = distanceSensor.getDistance();
+		data |= dist;
+		data |= health << 8;
+		health-=10;
+
+		hwlib::cout << "Distance: " << (data & 255) << hwlib::endl << "Health: " << ((data & 0xFF00) >> 8) << hwlib::endl << hwlib::endl;
+		transmitter.sendData(data);
+
+		hwlib::wait_ms(1000);
+		
+		if(health <= 0){
+			hwlib::cout << "You died..." << hwlib::endl;
+			break;
+		}
    	}
 }
