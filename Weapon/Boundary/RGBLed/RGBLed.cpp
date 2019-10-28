b4655ffc8b47777d5858de241c8add15fe9b1f70
1122joshua1 <joshua.vanree@student.hu.nl>
2019-10-28 14:52:28 +0100

    added pwm for 3 colors

--- Weapon/Boundary/RGBLed/RGBLed.cpp
+++ Weapon/Boundary/RGBLed/RGBLed.cpp
@@ -10,10 +10,37 @@
 /// echo_pin respectively.
 RGBLed::RGBLed(hwlib::color color):
 	currentcolor(color)
-{setColor(color);}
+{
+      PMC-> PMC_PCER1 |= PMC_PCER1_PID36;                     // Enable PWM
+  PIOA->PIO_ABSR |= PIO_ABSR_P19;                        // Set PWM pin perhipheral type A or B, in this case B
+  PIOA->PIO_PDR |= PIO_PA19;
+    PIOA->PIO_ABSR |= PIO_ABSR_P13;                        // Set PWM pin perhipheral type A or B, in this case B
+  PIOA->PIO_PDR |= PIO_PA13;
+      PIOC->PIO_ABSR |= PIO_ABSR_P9;                        // Set PWM pin perhipheral type A or B, in this case B
+  PIOC->PIO_PDR |= PIO_PC9;
+PWM->PWM_CH_NUM[1].PWM_CMR = PWM_CMR_CPRE_CLKA;
+PWM->PWM_CH_NUM[2].PWM_CMR = PWM_CMR_CPRE_CLKA;  //PWM-> PWM_CMP[0];                          // Set PWM pin to an output
+PWM->PWM_CH_NUM[3].PWM_CMR = PWM_CMR_CPRE_CLKA;
+  PWM->PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(42);     // Set the PWM clock rate to 2MHz (84MHz/42)
+
+  //WM->PWM_CMR.CLAG = PWM_CMR_CALG; //| PWM_CMR_CPRE_CLKA;      // Enable dual slope PWM and set the clock source as CLKA
+  PWM->PWM_CH_NUM[1].PWM_CPRD = 255;                                // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz
+  PWM->PWM_CH_NUM[1].PWM_CDTY = 1500;                                 // Set the PWM duty cycle to 1500 - centre the servo
+  PWM->PWM_ENA = PWM_ENA_CHID1;                          // Enable the PWM channel   
+    PWM->PWM_CH_NUM[2].PWM_CPRD = 255;                                // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz
+  PWM->PWM_CH_NUM[2].PWM_CDTY = 1500;                                 // Set the PWM duty cycle to 1500 - centre the servo
+  PWM->PWM_ENA = PWM_ENA_CHID2;   
+      PWM->PWM_CH_NUM[3].PWM_CPRD = 255;                                // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz
+  PWM->PWM_CH_NUM[3].PWM_CDTY = 255;                                 // Set the PWM duty cycle to 1500 - centre the servo
+  PWM->PWM_ENA = PWM_ENA_CHID3;  
+    setColor(color);}
 
 void RGBLed::setColor(hwlib::color color){
     currentcolor = color;
+    PWM->PWM_CH_NUM[2].PWM_CDTY = color.red;
+    PWM->PWM_CH_NUM[3].PWM_CDTY = color.blue;
+    PWM->PWM_CH_NUM[1].PWM_CDTY = color.green; 
+    /*
     if(color.green>127){
         greenpin.write(0);
     }
@@ -37,7 +64,7 @@ void RGBLed::setColor(hwlib::color color){
     {
         bluepin.write(1);
     }
-    /*
+    
     
      // enable the clock to port A
 	    PMC->PMC_PCER0 = 1 << ID_PIOA;
