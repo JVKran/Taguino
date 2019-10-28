60563539d83da24280747f059743fcef6dd4c452
1122joshua1 <joshua.vanree@student.hu.nl>
2019-10-28 15:27:36 +0100

    cleaned up code for RGB led

--- Weapon/Boundary/RGBLed/RGBLed.cpp
+++ Weapon/Boundary/RGBLed/RGBLed.cpp
@@ -27,7 +27,7 @@ PWM->PWM_CH_NUM[3].PWM_CMR = PWM_CMR_CPRE_CLKA;
 
   PWM->PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(22);     // Set the PWM clock rate to 2MHz (prescaler 84MHz/42)
 
-  //WM->PWM_CMR.CLAG = PWM_CMR_CALG; //| PWM_CMR_CPRE_CLKA;      // Enable dual slope PWM and set the clock source as CLKA
+
   PWM->PWM_CH_NUM[1].PWM_CPRD = 255;                                // Set the PWM frequency 2MHz/(2 * 255) = 3.9kHz
   PWM->PWM_CH_NUM[1].PWM_CDTY = 255;                                 // Set the PWM duty cycle to 255 max
   PWM->PWM_ENA = PWM_ENA_CHID1;                          // Enable the PWM channel   
@@ -41,64 +41,10 @@ PWM->PWM_CH_NUM[3].PWM_CMR = PWM_CMR_CPRE_CLKA;
 
 void RGBLed::setColor(hwlib::color color){
     currentcolor = color;
-    PWM->PWM_CH_NUM[2].PWM_CDTY = color.red;
-    PWM->PWM_CH_NUM[3].PWM_CDTY = color.blue;
-    PWM->PWM_CH_NUM[1].PWM_CDTY = color.green; 
-    /*
-    if(color.green>127){
-        greenpin.write(0);
-    }
-    else
-    {
-        greenpin.write(1);
-    }
-    
-    if(color.red>127){
-        redpin.write(0);
-    }
-    else
-    {
-        redpin.write(1);
-    }
+    PWM->PWM_CH_NUM[2].PWM_CDTY = color.red;                //Takes red value and puses it to the output register
+    PWM->PWM_CH_NUM[3].PWM_CDTY = color.blue;               //Takes blue value and puses it to the output register
+    PWM->PWM_CH_NUM[1].PWM_CDTY = color.green;              //Takes green value and puses it to the output register
 
-    if(color.blue>127){
-        bluepin.write(0);
-    }
-    else
-    {
-        bluepin.write(1);
-    }
-    
-    
-     // enable the clock to port A
-	    PMC->PMC_PCER0 = 1 << ID_PIOA;
-		
-	    // disable PIO Control on PA10 and PA11 and set up for Peripheral A
-		PIOA->PIO_PDR   = PIO_PA10; 
-		PIOA->PIO_ABSR |= PIO_PA8;  
-
-
-		// enable the clock to the USART
-	    PMC->PMC_PCER0 = ( 0x01 << ID_PWM );
-
-        PWM->PWM_WPCR = 0 << WPCMD;
-
-	    // Reset and disable receiver and transmitter.
-	    hw_usart->US_CR = US_CR_RSTRX | US_CR_RSTTX | US_CR_RXDIS | US_CR_TXDIS;
-
-	    // Set the baudrate to 250 000.
-
-	    hw_usart->US_BRGR = 21; 
-
-	    // No parity, normal channel mode.
-	    hw_usart->US_MR = US_MR_PAR_NO | US_MR_NBSTOP_2_BIT | US_MR_CHRL_8_BIT ;
-
-	    // Disable all interrupts.	  
-	    hw_usart->US_IDR = 0xFFFFFFFF;   
-
-	    // Enable the receiver and the trasmitter.
-	    hw_usart->US_CR = US_CR_RXEN | US_CR_TXEN;  
-      */  
 }
 
 
