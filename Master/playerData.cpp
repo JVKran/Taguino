beba3025eb10746e7d87427164118c92a36e4c3c
v1oopc-ubuntu <menno.vanderjagt@student.hu.nl>
2019-10-29 03:11:13 -0700

    new functions added

---
+++ Master/playerData.cpp
@@ -0,0 +1,67 @@
+#include "playerData.hpp"
+
+playerData::playerData(const char * name, const int playerNumber, const int teamNumber):
+	name(name),
+	playerNumber(playerNumber),
+	teamNumber(teamNumber)
+{}
+
+const char * playerData::getName() const {
+
+}
+
+uint8_t playerData::getPlayerNumber() const {
+
+}
+
+uint8_t playerData::getTeamNumber() const {
+
+}
+
+uint8_t playerData::getHealth() const {
+
+}
+
+void playerData::setHealth(uint8_t damageDone) {
+
+}
+
+uint16_t playerData::getShots() const {
+
+}
+
+void playerData::setShots(){
+
+}
+
+uint16_t playerData::getDamage() const {
+
+}
+
+void playerData::setDamage(uint8_t damageDealt) {
+
+}
+
+uint8_t playerData::getKills() const {
+
+}
+
+void playerData::setKills() {
+
+}
+
+uint8_t playerData::getDeaths() const {
+
+}
+
+void playerData::setDeaths() {
+
+}
+
+uint16_t playerData::getScore() const {
+
+}
+
+void playerData::setScore(uint8_t newShot){
+	
+}
