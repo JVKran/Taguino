3abfe075a98e07616de3e57b18807c47e2c128b7
v1oopc-ubuntu <menno.vanderjagt@student.hu.nl>
2019-10-29 07:01:42 -0700

    declaring some functions

--- Master/Control/runGame.hpp
+++ Master/Control/runGame.hpp
@@ -6,5 +6,18 @@ private:
 	rtos::channel receiveMessageChannel;
 
 	std::array<playerData, 32> players = {};
+	rotaryEncoder& encoder;
+	keypad& keypad;
+
+public:
+	runGame();
+
+	void damageDone(uint8_t damage);
+	bool keyPressed();
+	bool encoderPressed();
+	void encoderTurnedToPos(int pos);
+	void startGame();
+	void decodeMessage();
+	void main();
 
 }
