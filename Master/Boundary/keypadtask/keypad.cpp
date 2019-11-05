eda8c85067a422bbe4f3f69246436769bacbecb7
1122joshua1 <joshua.vanree@student.hu.nl>
2019-11-05 14:59:44 +0100

    made keypad

---
+++ Master/Boundary/keypadtask/keypad.cpp
@@ -0,0 +1,162 @@
+
+#include "keypad.hpp"
+    void testlistner::dataReceived(char karakter){
+        hwlib::cout<<karakter<<hwlib::endl;
+    }
+
+
+T9Keys::T9Keys(keypadListener* listen,unsigned long long int period, const char * name):
+	task(name),
+	updateClock(this, period, "Update Clock"),
+    listen(listen)
+{}
+
+
+
+
+
+void T9Keys::main(){
+    char karakter =0;
+    char i;
+    uint8_t count=0;
+    bool returned= false;
+    int multiplier=0;
+    int maxcount=2;
+    uint_fast64_t lastpress= hwlib::now_us();
+	for(;;){
+		wait(updateClock);
+        if(((hwlib::now_us()-lastpress) > 1000000)&&returned==false){
+            listen->dataReceived(karakter);
+            returned=true;
+        }
+        else{
+        if(numpad.char_available()){
+            hwlib::wait_ms(10);
+            if((hwlib::now_us()-lastpress) > 1000000){
+                returned= false;
+                count=0;
+
+                
+                i=numpad.getc();
+                lastpress=hwlib::now_us();
+                if(i=='7'){
+                    multiplier=0;
+                    maxcount= 3;
+                }
+                else if(i =='8'){
+                    maxcount=2;
+                    multiplier=1;
+                }
+                else if(i=='9')
+                {
+                    multiplier=1;
+                    maxcount=3;
+                }
+                
+                else
+                {
+                    maxcount=2;
+                    multiplier=0;
+                }
+                if(int(i)>0){
+                    karakter= (((i - '0') - 2) * 3) + 97+multiplier;
+                    //hwlib::cout<<karakter<<hwlib::endl;
+                }
+        }
+        else
+        {
+            hwlib::wait_ms(10);
+
+            if((numpad.getc()==i)&&(count<maxcount)){
+                count++;
+                karakter+=1;
+                //hwlib::cout<<karakter<<hwlib::endl;
+
+            }
+            else{
+                lastpress=0;
+            }
+        
+
+
+
+        }
+        }
+    }
+    }
+        
+        
+
+}
+
+
+
+
+char T9Keys::readChar(){
+    
+	//if(numpad.char_available()){
+
+        return 0;
+        
+	// 	newCharacter = numpad.getc();
+	// 	keyPressPeriod = hwlib::now_us() - lastKeyPress;
+	// 	lastKeyPress = hwlib::now_us();
+	// 	lastKey = newCharacter;
+	// 	lastCharacter = (((newCharacter - '0') - 2) * 3) + 97;
+	// 	//hwlib::wait_ms(200);
+	// 	// while(hwlib::now_us() - lastKeyPress < 500'000){
+	// 	// 	if(numpad.char_available()){
+	// 	// 		newCharacter = numpad.getc();
+	// 	// 		if(newCharacter != '\0' && letterIncrements < 2 && newCharacter == lastKey && newCharacter < '7'){
+	// 	// 			lastCharacter++;
+	// 	// 			letterIncrements++;
+	// 	// 			lastKeyPress = hwlib::now_us();
+	// 	// 			hwlib::wait_ms(100);
+	// 	// 		} else if(newCharacter == '7' && letterIncrements < 3 && newCharacter == lastKey){
+	// 	// 			lastCharacter++;
+	// 	// 			letterIncrements++;
+	// 	// 			lastKeyPress = hwlib::now_us();
+	// 	// 			hwlib::wait_ms(100);
+	// 	// 		} else if(newCharacter == '8' && letterIncrements < 2 && newCharacter == lastKey){
+	// 	// 			lastCharacter++;
+	// 	// 			letterIncrements++;
+	// 	// 			lastKeyPress = hwlib::now_us();
+	// 	// 			hwlib::wait_ms(100);
+	// 	// 		} else if(newCharacter == '9' && letterIncrements < 3 && newCharacter == lastKey){
+	// 	// 			lastCharacter++;
+	// 	// 			letterIncrements++;
+	// 	// 			lastKeyPress = hwlib::now_us();
+	// 	// 			hwlib::wait_ms(100);
+	// 	// 		} else {
+	// 	// 			break; 
+	// 	// 		}
+	// 	// 	}
+	// 	// }
+	// 	letterIncrements = 0;
+	// 	return lastCharacter;
+	// } else {
+	// 	return 0;
+	// }
+}
+
+/// \brief
+/// Read Key
+/// \details
+/// This function returns the character of the pressed key equal to the one drawn on the numpad.
+char T9Keys::readKey(){
+	return numpad.pressed();
+}
+
+/// \brief
+/// Debug
+/// \details
+/// This function can be used for debugging. It prints the amount of presses that have been done
+/// and the pressed key.
+void T9Keys::debug(){
+   	int n = 0;
+   	for(;;){
+	   	if(numpad.char_available()){
+	   		hwlib::cout << n++ << " [" << numpad.getc() << "]\n";
+	   	}
+   	}
+}
---
+++ Master/Boundary/keypadtask/keypad.hpp
@@ -0,0 +1,63 @@
+#ifndef __T9_KEYS_HPP
+#define __T9_KEYS_HPP
+
+#include "hwlib.hpp"
+#include "rtos.hpp"
+class keypadListener {
+public:
+    virtual void dataReceived(char karakter) = 0;
+};
+class testlistner : public keypadListener{
+     void dataReceived(char karakter);
+};
+
+class T9Keys : public rtos::task<> {
+	private:
+
+
+
+		hwlib::target::pin_oc out0 = hwlib::target::pin_oc( hwlib::target::pins::d51 );
+		hwlib::target::pin_oc out1 = hwlib::target::pin_oc( hwlib::target::pins::d49);
+		hwlib::target::pin_oc out2 = hwlib::target::pin_oc( hwlib::target::pins::d47 );
+		hwlib::target::pin_oc out3 = hwlib::target::pin_oc( hwlib::target::pins::d45 );
+
+		hwlib::target::pin_in in0  = hwlib::target::pin_in( hwlib::target::pins::d43 );
+		hwlib::target::pin_in in1  = hwlib::target::pin_in( hwlib::target::pins::d41);
+		hwlib::target::pin_in in2  = hwlib::target::pin_in( hwlib::target::pins::d39);
+		hwlib::target::pin_in in3  = hwlib::target::pin_in( hwlib::target::pins::d37 );
+		
+		hwlib::port_oc_from_pins_t  out_port = hwlib::port_oc_from_pins_t( out0, out1, out2, out3 );
+		hwlib::port_in_from_pins_t  in_port  = hwlib::port_in_from_pins_t( in0,  in1,  in2,  in3  );
+		hwlib::matrix_of_switches   matrix   = hwlib::matrix_of_switches( out_port, in_port );
+   		hwlib::keypad<16> numpad   = hwlib::keypad< 16 >( matrix, "147*2580369#ABCD" );
+
+		uint_fast64_t lastKeyPress;
+		uint_fast64_t keyPressPeriod;
+
+		char lastCharacter;				//Lastly returned char; after T9-Simulation.
+		char lastKey;					//Lastly pressed key; before T9-Simulation.
+		char newCharacter;				//Newly received char; before T9-Simulation (result of getc())
+		unsigned int letterIncrements;	//Amount of double presses; equal to amount of increments (from a to b and c).
+
+        char karakter =0;
+        char i;
+        uint8_t count=0;
+        uint_fast64_t lastpress= hwlib::now_us();
+
+
+		rtos::clock updateClock;
+                keypadListener* listen;
+	public:
+		T9Keys(keypadListener* listen,unsigned long long int period = 10'000, const char * name = "Input Handler");
+
+		char readChar();
+		char readKey();
+
+		void debug();
+
+		
+
+		void main() override;
+};
+
+#endif //__INPUT_HPP
