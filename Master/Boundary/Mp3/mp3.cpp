344c7aba07a658f74eea6fc791d856a1a26c8a43
FaizalSupriadi <faizal.faizalrachmansupriadi@student.hu.nl>
2019-11-06 10:33:44 +0100

    Added mp3 and implemented in signup.cpp/hpp

---
+++ Master/Boundary/Mp3/mp3.cpp
@@ -0,0 +1,83 @@
+///@file
+#include "mp3.hpp"
+
+hwuart::hwuart(){
+
+		    // enable the clock to port A
+	    PMC->PMC_PCER0 = 1 << ID_PIOA;
+		
+	    // disable PIO Control on PA10 and PA11 and set up for Peripheral A
+		PIOA->PIO_PDR   = PIO_PA10; 
+		PIOA->PIO_ABSR &= ~PIO_PA10; 
+		PIOA->PIO_PDR   = PIO_PA11; 
+		PIOA->PIO_ABSR &= ~PIO_PA11; 
+
+		// enable the clock to the USART
+	    PMC->PMC_PCER0 = ( 0x01 << ID_USART0 );
+
+	    // Reset and disable receiver and transmitter.
+	    USART0->US_CR = US_CR_RSTRX | US_CR_RSTTX | US_CR_RXDIS | US_CR_TXDIS;
+
+	    // Set the baudrate to 9600.
+
+	    USART0->US_BRGR = 567; 
+
+	    // No parity, normal channel mode.
+	    USART0->US_MR = US_MR_PAR_NO | US_MR_NBSTOP_1_BIT | US_MR_CHRL_8_BIT;
+
+	    // Disable all interrupts.	  
+	    USART0->US_IDR = 0xFFFFFFFF;   
+
+	    // Enable the receiver and the trasmitter.
+	    USART0->US_CR = US_CR_RXEN | US_CR_TXEN;      
+	}
+
+	bool hwuart::usart_char_available(){
+	   	
+	   return ( USART0->US_CSR & 1 ) != 0;
+	}
+
+	char hwuart::usart_getc(){
+	   // uart_init() is not needed because uart_char_available does that
+	   //while( ! usart_char_available() ){}
+	   return USART0->US_RHR; 
+	}
+	void hwuart::usart_putc( char c ){
+	      
+	   //usart_init();	
+	   while( ( USART0->US_CSR & 2 ) == 0 ){}
+	   USART0->US_THR = c;
+
+	}
+
+void send::executeCMD( uint8_t CMD, uint8_t par1, uint8_t par2 ){ 		// Command, folder(in numbers 1-99), song(0-255)
+	// Standard has the start, version, length, cmd, ack, par1, par2, check, check, end bytes
+	uint8_t standard[10] = { 0x7E, 0xFF, 0x06, CMD, 0x00, par1, par2, 0xFF, 0xFF, 0xEF };
+	
+	uint16_t checksum = -( standard[1] + standard[2] + standard[3] + standard[4] + standard[4] + standard[5]+ standard[6] );
+		
+	// The bytes we will send to the mp3 module
+	uint8_t commandLine[10] = { standard[0], standard[1], standard[2], standard[3], standard[4], standard[5], standard[6], uint8_t(checksum >> 8), uint8_t (checksum & 0xFF), 0xEF };
+	hwlib::wait_ms(100);
+		
+	// We send via the usart_putc() function
+	for(int i=0; i<10; i++){
+			usart_putc(commandLine[i]);
+			hwlib::wait_us(100);
+			
+		}
+	// Wait 4 seconds, because the module needs 3-5 seconds to process the information.
+	// OR don't have this but don't press another song in 4 seconds.
+	//hwlib::wait_ms(4000);
+}
+
+void mp3::sendCMD(uint8_t CMD, uint8_t par1, uint8_t par2){ executeCMD( CMD, par1, par2 );}
+
+void mp3::playSound( int sound ){ executeCMD( 3, 0, sound );  }			// playSounds = ( specify track , folder 0, number with corresponding mp3 track )
+
+void mp3::pauseSound(){ executeCMD( 0x0E, 0, 0 );}					    // Pause
+
+void mp3::setVolume( int volume ){ executeCMD( 6, 0, volume); };		// setVolume = (set volume, nothing, volume range 0-30)
+
+void mp3::resetModule(){ executeCMD( 0x0C, 0, 0 );}						// Reset the module 
+			
---
+++ Master/Boundary/Mp3/mp3.hpp
@@ -0,0 +1,140 @@
+///@file
+#ifndef _MP3_HPP
+#define _MP3_HPP
+
+#include <array>
+#include "hwlib.hpp"
+/// \brief
+/// This sounds class has a template "<int, N>" and an array called sound where songs are put in.
+/// \details
+/// The Array holds N amount of ints, these ints are used to play a song.
+/// This class has 3 functions:
+///		- play(int i): 	The play function returns the given sound on its position.	
+///		- size(): 		The size function returns the size of the sound array.
+///		- set(): 		The set function sets the given value.
+
+template< int N >
+class sounds{
+private:
+	std::array< int, N > sound;
+public:
+/// \brief
+/// Function to return from array sound.
+/// \details
+/// This function is used to return a value from sound at position i - 1.
+	int play( int i ){ return sound[ i-1 ]; }
+/// \brief
+/// Function to return size of sound.
+/// \details
+/// This function returns the size of the sound array.
+	int size(){ return sound.size();}
+/// \brief
+///	Function to set a sound.
+/// \details
+/// This function will set the given value at a new position.
+	void set( int setSound ){ sound[setSound-1] = setSound; }
+
+};
+
+class hwuart{
+	//Usart * hw_usart = USART0;
+	public:
+	hwuart();
+	bool usart_char_available();
+	char usart_getc();
+	void usart_putc(char c);
+};
+
+/// \brief
+///	The class send inherits from hwuart and is used to send commands to the mp3 module.
+/// \details
+///	The class has one function:
+/// 	- executeCMD( uint8_t CMD, uint8_t par1, uint8_t par2): 
+///     	This needs the mp3 command, command specific value, command specific value2 and will then send commands to the mp3 module.
+class send : public hwuart{
+public:
+/// \brief
+///	Function to execute commands to the mp3 module
+/// \details
+/// This needs the mp3 command, folder and mp3 file. It will send commands to the mp3 module.
+	void executeCMD( uint8_t CMD, uint8_t par1, uint8_t par2);
+};
+
+/// \brief
+///	The mp3 class inherits from send and is used to specify which commands need to be sent
+/// \details
+///	The class has 5 function:
+/// 	- sendCMD(uint8_t CMD, uint8_t par1, uint8_t par2): Send your own commands.
+/// 	- resetModule(): 			Reset the mp3 module.
+/// 	- playSound( int sound ): 	Play the given sound.
+/// 	- pauseSound(): 			Pause the sound.
+/// 	- setVolume( int volume ): 	Set the volume.
+class mp3 : public send{
+public:
+/// \brief
+/// sendCMD sends your own commands.
+/// \details
+/// This function will send the command, command specific value, command specific value2 and executes it at the mp3 module.
+	void sendCMD( uint8_t CMD, uint8_t par1, uint8_t par2 );
+/// \brief
+/// resetModule will reset the mp3 module.
+/// \details
+/// This function will send the command to reset to the mp3 module, 
+/// clearing every command you have given to the module.
+	void resetModule();
+/// \brief
+/// playSound will play the given sound.
+/// \details
+///	This function will send the command to play the given sound from the beginning.
+/// Folders on the SD are numbered from 1-99 and mp3 files are numbered from 1 - 255, 
+/// giving those values will play the corresponding sound. In this case the folder is always the first one.
+	void playSound( int sound );
+/// \brief
+/// pauseSound will pause the current sound.
+/// \details
+/// This function will pause the sound that is currently playing,
+/// altough there is no start function, meaning this can also be seen as stopping a sound.
+	void pauseSound();
+/// \brief
+/// setVolume will set the volume at given value.
+/// \details
+/// This function will set the volume of the mp3 module at the given value,
+/// the volume can go from 0 - 30.
+	void setVolume( int volume ); //Van 0-30
+};
+
+
+/// \brief
+/// The mp3Control class inherits mp3 and sounds<N>, it has a template "<int N>" and is used to play songs for given data in NRF
+/// \details
+/// This initializes the sound array and has 1 function:
+/// 	- startPlayingSound(int data): If data is in the sound array, play it.
+
+template< int N >
+class mp3Control : public mp3, public sounds<N>{
+private:
+	int JAZZ=1; int ACTION=2; 
+	std::array< int, N > soundTmp = { JAZZ, ACTION };
+	sounds<N> sound;
+public:
+	mp3Control()
+	{
+		for( const auto &s : soundTmp){
+			sound.set(s);
+		}
+	}
+/// \brief
+/// startPlayingSound will play a sound corresponding to the given data.
+/// \details
+/// This function will compare the data to the values in the array, if they are the same it will play the corresponding sound.
+	void startPlayingSound( int data ){
+		for( int i=1; i<sound.size()+1; i++){ 
+			if( data == i ){ 			// If data is a song, play it. +1 because .play will return value -1;
+				playSound( sound.play( i ) );
+				break;
+			}
+		}
+	}
+};
+
+#endif //MP3_HPP
